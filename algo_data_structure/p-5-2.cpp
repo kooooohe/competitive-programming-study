7f6c44f70cd4b9c7ebde97b7eee295223f3c6856
kooooohe <1211lta+github@gmail.com>
2021-02-21 00:37:01 +0900

    p-5-2

--- algo_data_structure/p-5-2.cpp
+++ algo_data_structure/p-5-2.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-vector<vector<long long>> data;
+vector<vector<long long>> scores;
+vector<vector<long long>> memo;
 int rec(int n, int use) {
   if (n < 0) {
     return 0;
@@ -11,27 +12,31 @@ int rec(int n, int use) {
   int a1 = -1;
   int a2 = -1;
   int a3 = -1;
-
-  if (use != 0b001) {
-    a1 = data[n][0] + rec(n - 1, 0b001);
-  }
-  if (use != 0b010) {
-    a2 = data[n][1] + rec(n - 1, 0b010);
-  }
-  if (use != 0b100) {
-    a3 = data[n][2] + rec(n - 1, 0b100);
+  if (memo[n][use] > -1) {
+	return memo[n][use];
   }
 
-  return max(a1, max(a2, a3));
+  if (use != 0) {
+      a1 = scores[n][0] + rec(n - 1, 0);
+  }
+  if (use != 1) {
+      a2 = scores[n][1] + rec(n - 1, 1);
+  }
+  if (use != 2) {
+      a3 = scores[n][2] + rec(n - 1, 2);
+  }
+
+  return memo[n][use] = max(a1, max(a2, a3));
 }
 
 int main() {
   int N;
   cin >> N;
-  // vector<vector<long long>> data(N, vector<long long>(3));
-  data.assign(N, vector<long long>(3));
+  // vector<vector<long long>> scores(N, vector<long long>(3));
+  scores.assign(N, vector<long long>(3));
+  memo.assign(N, vector<long long>(4, -1));
   for (int i = 0; i < N; ++i) {
-    cin >> data[i][0] >> data[i][1] >> data[i][2];
+    cin >> scores[i][0] >> scores[i][1] >> scores[i][2];
   }
-  cout << rec(N - 1, 0) << endl;
+  cout << rec(N - 1, 3) << endl;
 }
