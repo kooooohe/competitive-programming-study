70a3a7ae357e7ea0a860f6bf97139e5419f811ac
kooooohe <1211lta+github@gmail.com>
2021-02-20 22:02:58 +0900

    5-9

---
+++ algo_data_structure/5-9.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T> void chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+  }
+}
+
+const long long INF = 1LL << 60;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<vector<long long>> c(N + 1, vector<long long>(N + 1));
+
+  for (int i = 0; i < N + 1; ++i) {
+    for (int j = 0; j < N + 1; ++j) {
+      cin >> c[i][j];
+    }
+  }
+
+  vector<long long> dp(N + 1, INF);
+  dp[0] = 0;
+
+  for (int i = 0; i <= N; ++i) {
+    for (int j = 0; j < i; ++i) {
+	chmin(dp[i], dp[j]+c[j][i]);
+    }
+  }
+  cout << dp[N] << endl;
+}
