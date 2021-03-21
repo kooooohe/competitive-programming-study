5750aa6a198f9d4144e907bed79b0e6b3b49fa0f
kooooohe <1211lta+github@gmail.com>
2021-03-21 18:12:21 +0900

    p-5-9

--- algo_data_structure/p-5-9.cpp
+++ algo_data_structure/p-5-9.cpp
@@ -2,5 +2,38 @@
 #include <vector>
 
 using namespace std;
+template <class T> void chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+  }
+}
 
-int main() {}
+long long INF = 1LL << 60;
+int main() {
+  int N;
+  cin >> N;
+  vector<long long> a(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> a[i];
+  }
+
+  vector<long long> S(N + 1, 0);
+  for (int i = 0; i < N; ++i) {
+    S[i + 1] = S[i] + a[i];
+    // cout << S[i+1] << endl;
+  }
+  vector<vector<long long>> dp(N + 1, vector<long long>(N + 1, INF));
+
+  for (int i = 0; i < N; ++i) {
+    dp[i][i + 1] = 0;
+  }
+  for (int bet = 2; bet <= N; ++bet) {
+    for (int i = 0; i + bet <= N; ++i) {
+      int j = i + bet;
+      for (int k = i+1; k < j; ++k) {
+        chmin(dp[i][j], dp[i][k] + dp[k][j] + (S[j] - S[i]));
+      }
+    }
+  }
+  cout << dp[0][N] << endl;
+}
