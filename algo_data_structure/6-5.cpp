b44c86995ab9f723085dde9a0432764f1278245e
kooooohe <1211lta+github@gmail.com>
2021-03-28 16:31:00 +0900

    6-5

---
+++ algo_data_structure/6-5.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const long long INF = 1LL << 60;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<long long> h(N);
+  vector<long long> s(N);
+
+  for (int i = 0; i < N; ++i) {
+    cin >> h[i] >> s[i];
+  }
+
+  long long left = 0;
+  long long right = INF;
+
+  while (right - left > 1) {
+    long long mid = (right + left) / 2;
+
+    bool ok = true;
+    vector<long long> t(N, 0);
+
+    for (int i = 0; i < N; ++i) {
+      if (mid < h[i]) {
+        ok = false;
+      } else {
+        t[i] = (mid - h[i]) / s[i];
+      }
+    }
+    sort(t.begin(), t.end());
+
+    for (int i = 0; i < N; ++i) {
+      if (t[i] < i) {
+        ok = false;
+      }
+    }
+    if (ok) {
+	    right = mid;
+    } else {
+	    left = mid;
+    }
+  }
+
+  cout << right << endl;
+}
