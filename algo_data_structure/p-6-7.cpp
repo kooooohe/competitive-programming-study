408f1c74582b22f1c6262da8077f95d47170f4b7
kooooohe <1211lta+github@gmail.com>
2021-05-08 15:53:34 +0900

    p-6-7

--- algo_data_structure/p-6-7.cpp
+++ algo_data_structure/p-6-7.cpp
@@ -3,12 +3,13 @@
 
 using namespace std;
 
+
 template <typename T> struct BIT {
   int n;
   vector<T> bit;
   BIT(int n_) {
     n = n_ + 1;
-    bit = bit(n, 0);
+    bit.assign(n, 0);
   };
 
   void add(int a, T x) {
@@ -24,6 +25,48 @@ template <typename T> struct BIT {
     }
     return s;
   }
+
+  void debug() {
+    for (int i = 1; i < n; i++) {
+      cout << bit[i] << " ";
+    }
+    cout << endl;
+  }
 };
 
-int main() {}
+int main() {
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> a[i];
+  }
+  int low = 0;
+  int high = 1 << 30;
+  const int geta = N + 1;
+  while (high - low > 1) {
+    int mid = (high + low) / 2;
+    BIT<long long> bit((N + 1) * 2);
+    bit.add(geta, 1);
+    int sum = 0;
+    long long num = 0;
+    for (int i = 0; i < N; ++i) {
+      if (a[i] <= mid) {
+        sum += 1;
+      } else {
+        sum -= 1;
+      }
+      bit.debug();
+      num += bit.sum(sum + geta -1);
+      bit.add(sum + geta, 1);
+    }
+    cout  << endl;
+
+    if (num >= N*(N+1)/(2*2)) {
+        high = mid;
+    } else {
+        low = mid;
+    }
+  }
+  cout << high << endl;
+}
